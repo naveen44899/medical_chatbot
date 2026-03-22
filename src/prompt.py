ce4bcd13655b738c568b9a86b5f9a3309bf2cc32
naveen kumar <naveen8296088066@gmail.com>
2026-03-22 13:06:31 +0530

    created app.py

---
+++ src/prompt.py
@@ -0,0 +1,11 @@
+
+
+system_prompt = (
+    "you are an Medical assistent for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. if you dont't know the answer, say that you "
+    "don't Know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
---
+++ store_index.py
@@ -0,0 +1,40 @@
+from langchain_pinecone import PineconeVectorStore
+from dotenv import load_dotenv
+import os
+from src.helper import load_pdf_files,filter_to_minimal_docs,text_split,download_embeddings
+from  pinecone import Pinecone
+from pinecone import ServerlessSpec
+
+
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+groq_api = os.getenv("GROQ_API_KEY")
+
+
+extracted_data= load_pdf_files("data")
+minimal_docs = filter_to_minimal_docs(extracted_data)
+text_chunks = text_split(minimal_docs)
+
+embeddings = download_embeddings()
+
+pineconedb = Pinecone(api_key=PINECONE_API_KEY)
+index_name = "medical-chatbot"
+
+if not pineconedb.has_index(index_name):
+    pineconedb.create_index(
+        name=index_name,
+        dimension=384,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws",region="us-east-1")
+    )
+
+
+index = pineconedb.Index(index_name)
+
+
+docsearch = PineconeVectorStore.from_documents(
+    documents=text_chunks,
+    embedding=embeddings,
+    index_name=index_name
+)
